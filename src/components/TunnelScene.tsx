bd6fdc54051427fcd279ba6d6c706a601baf48e0
trixvenom12 <adityar150805@gmail.com>
2026-02-20 23:30:50 +0530

    fixing tunnel

--- src/components/TunnelScene.tsx
+++ src/components/TunnelScene.tsx
@@ -1,30 +1,35 @@
 import React, { useMemo } from 'react';
 import { Canvas } from '@react-three/fiber';
 import { Image, Text, Float } from '@react-three/drei';
-import * as THREE from 'three';
 
-// FOV 80 creates the compressed cinematic perspective seen in the reference
-const FOV_WARP = 80; 
+// A tighter FOV prevents the extreme stretching on the edges
+const FOV_WARP = 65; 
 
-const TUNNEL_DATA = [
-  // LEFT SIDE - Dynamic spacing to avoid the "boxy" look
-  { pos: [-10, 2, 12],   rot: [0, Math.PI / 4, 0],   scale: [9, 6] }, 
-  { pos: [-11, -3, 6],   rot: [0, Math.PI / 3, 0],   scale: [8, 5] },
-  { pos: [-12, 4, -2],   rot: [0, Math.PI / 2, 0],   scale: [10, 6] },
-  { pos: [-11, -1, -10], rot: [0, Math.PI / 2.5, 0], scale: [7, 5] },
-  { pos: [-10, 5, -18],  rot: [0, Math.PI / 3, 0],   scale: [9, 6] },
+// Placed to mimic the exact scatter pattern in your Framer screenshot
+const SCATTER_DATA = [
+  // Top Left cluster
+  { pos: [-8, 4, 2], rot: [0.2, Math.PI / 6, 0], scale: [7, 4.5] },
+  { pos: [-10, 1, 5], rot: [0.1, Math.PI / 5, 0], scale: [6, 4] },
+  
+  // Bottom Left cluster
+  { pos: [-9, -4, 4], rot: [-0.2, Math.PI / 6, 0], scale: [7, 4.5] },
+  { pos: [-5, -6, 0], rot: [-0.3, Math.PI / 8, 0], scale: [8, 5] },
+  
+  // Top Right cluster
+  { pos: [8, 5, 0], rot: [0.2, -Math.PI / 6, 0], scale: [8, 5] },
+  { pos: [10, 1, 6], rot: [0.1, -Math.PI / 5, 0], scale: [6, 4] },
+  
+  // Bottom Right cluster
+  { pos: [9, -4, 2], rot: [-0.2, -Math.PI / 6, 0], scale: [7, 4.5] },
+  { pos: [6, -7, 0], rot: [-0.3, -Math.PI / 8, 0], scale: [8, 5] },
 
-  // RIGHT SIDE - Overlapping depths for a collage feel
-  { pos: [10, 3, 10],    rot: [0, -Math.PI / 4, 0],  scale: [9, 6] }, 
-  { pos: [11, -4, 4],    rot: [0, -Math.PI / 3, 0],  scale: [8, 5.5] },
-  { pos: [12, 1, -5],    rot: [0, -Math.PI / 2, 0],  scale: [11, 7] },
-  { pos: [11, 5, -12],   rot: [0, -Math.PI / 2.5, 0], scale: [8, 5] },
-
-  // TOP & BOTTOM - Filling the peripheral vision
-  { pos: [0, 8, 5],      rot: [Math.PI / 2, 0, 0],   scale: [12, 8] }, 
-  { pos: [-5, -8, 2],    rot: [-Math.PI / 2, 0, 0],  scale: [10, 6] },
-  { pos: [5, -8, -8],    rot: [-Math.PI / 2, 0, 0],  scale: [9, 6] },
-  { pos: [0, 8, -20],    rot: [Math.PI / 2, 0, 0],   scale: [8, 5] },
+  // Center-ish Top & Bottom
+  { pos: [0, 6, -5], rot: [0.4, 0, 0], scale: [9, 5] },
+  { pos: [0, -6, -2], rot: [-0.4, 0, 0], scale: [8, 5] },
+  
+  // Far Background for depth
+  { pos: [-4, 2, -10], rot: [0, 0.2, 0], scale: [5, 3] },
+  { pos: [4, -2, -12], rot: [0, -0.2, 0], scale: [6, 3.5] },
 ];
 
 interface StaticFixedImageProps {
@@ -33,63 +38,35 @@ interface StaticFixedImageProps {
 }
 
 const StaticFixedImage = ({ data, index }: StaticFixedImageProps) => {
-  const url = useMemo(() => `https://picsum.photos/800/500?random=${index + 200}`, [index]);
+  const url = useMemo(() => `https://picsum.photos/800/500?random=${index + 300}`, [index]);
   
   return (
-    <Float speed={2} rotationIntensity={0.05} floatIntensity={0.1}>
+    <Float speed={1.5} rotationIntensity={0.1} floatIntensity={0.2}>
       <group 
         position={data.pos as [number, number, number]} 
         rotation={data.rot as [number, number, number]}
       >
+        {/* Borders are gone. Opacity is 1 for vibrant colors. */}
         <Image 
           url={url} 
           scale={[data.scale[0], data.scale[1]]} 
           transparent 
-          opacity={0.85}
-          color="#999999" // Tinting images darker ensures the central white text "pops"
+          opacity={1} 
           toneMapped={false}
         />
-        <lineSegments>
-           <edgesGeometry args={[new THREE.PlaneGeometry(data.scale[0], data.scale[1])]} />
-           <lineBasicMaterial color="#333" transparent opacity={0.3} />
-        </lineSegments>
       </group>
     </Float>
   );
 };
 
-const WireframeTube = () => {
-  const size = 150;
-  const divisions = 60;
-  // Offsets are wider than the image positions to stop the grid from clipping through images
-  const wallOffset = 14.5; 
-  const floorOffset = 10.5;
-
-  return (
-    <group position={[0, 0, -30]}>
-      <gridHelper args={[size, divisions, "#111", "#050505"]} position={[0, -floorOffset, 0]} />
-      <gridHelper args={[size, divisions, "#111", "#050505"]} position={[0, floorOffset, 0]} />
-      <gridHelper 
-        args={[size, divisions, "#111", "#050505"]} 
-        position={[-wallOffset, 0, 0]} 
-        rotation={[0, 0, Math.PI / 2]} 
-      />
-      <gridHelper 
-        args={[size, divisions, "#111", "#050505"]} 
-        position={[wallOffset, 0, 0]} 
-        rotation={[0, 0, Math.PI / 2]} 
-      />
-    </group>
-  );
-};
-
 const CentralLogo = () => {
   return (
-    <group position={[0, 0, -25]}> 
+    <group position={[0, 0, -8]}> 
       <Text
         font="https://fonts.gstatic.com/s/inter/v12/UcCO3FwrK3iLTeHuS_fvQtMwCp50KnMw2boKoduKmMEVuLyfAZ9hjp-Ek-_EeA.woff"
-        fontSize={6.5}
-        scale={[1.25, 0.85, 1]} // Squashing height + extending width for that heavy cinematic logo
+        fontSize={5}
+        // X-scale at 1.4 forces the font to be ultra-wide and blocky
+        scale={[1.4, 1, 1]} 
         letterSpacing={-0.08}
         color="white"
         fontWeight={900}
@@ -97,9 +74,9 @@ const CentralLogo = () => {
         PANORAMA
       </Text>
       <Text
-        fontSize={0.7}
-        position={[0, -3.4, 0]}
-        letterSpacing={1.1} 
+        fontSize={0.6}
+        position={[0, -2.5, 0]}
+        letterSpacing={1.5} 
         color="white"
         fontWeight={400}
       >
@@ -113,20 +90,14 @@ export default function TunnelScene() {
   return (
     <div style={{ width: '100vw', height: '100vh', background: 'black' }}>
       <Canvas 
-        camera={{ position: [0, 0, 20], fov: FOV_WARP }} 
-        gl={{ 
-            antialias: true,
-            logarithmicDepthBuffer: true // Fixes flickering for overlapping 3D objects
-        }}
+        camera={{ position: [0, 0, 15], fov: FOV_WARP }} 
       >
         <color attach="background" args={['#000000']} />
-        {/* Fog depth ensures background elements fade perfectly into the black background */}
-        <fog attach="fog" args={['#000000', 5, 55]} />
-        <ambientLight intensity={0.5} />
+        <fog attach="fog" args={['#000000', 10, 40]} />
         <group>
-          <WireframeTube />
+          {/* WireframeTube is completely deleted. It was ruining the render. */}
           <CentralLogo />
-          {TUNNEL_DATA.map((data, i) => (
+          {SCATTER_DATA.map((data, i) => (
             <StaticFixedImage key={i} data={data} index={i} />
           ))}
         </group>
